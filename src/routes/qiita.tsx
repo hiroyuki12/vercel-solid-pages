416e3d828ea69015c004130b9a802189ae9de769
hiroyuki12 <hiroyuki12@gmail.com>
2022-05-17 22:35:09 +0900

    Update set page Qiita.

--- src/routes/qiita.tsx
+++ src/routes/qiita.tsx
@@ -6,10 +6,10 @@ import moment from "moment";
 import "./QiitaApp.css";
 
 const fetchData = async (skip: number) =>
-  (await fetch(`https://qiita.com/api/v2/tags/react/items`)).json();
+  (await fetch(`https://qiita.com/api/v2/tags/react/items?page=${skip}`)).json();
 
 const CatsPage: Component = () => {
-  const [skip, setSkip] = createSignal(0);
+  const [skip, setSkip] = createSignal(1);
   const [data, { refetch }] = createResource<Qiita[], number>(skip, fetchData);
 
   let input!: HTMLInputElement;
@@ -31,7 +31,7 @@ const CatsPage: Component = () => {
         <a className="QiitaApp-link" href="https://mbp.hatenablog.com/entry/2021/07/15/214300" target="_blank">ReactでQiita APIから記事情報を取得して表示</a>
       </p>
       <input type="number" placeholder="Enter Skip Number" ref={input} />
-      <button onClick={() => onSetSkip()}>set skip</button>
+      <button onClick={() => onSetSkip()}>set page</button>
       <button onClick={() => refetch()}>refetch</button>
       <Suspense fallback={<div>Loading...</div>}>
         <ul>
