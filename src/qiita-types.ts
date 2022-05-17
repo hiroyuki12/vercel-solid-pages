ac8e66fd1c2329ca051395e768aae2077632f0e4
hiroyuki12 <hiroyuki12@gmail.com>
2022-05-17 19:58:36 +0900

    Add Qiita PAGE.

---
+++ src/qiita-types.ts
@@ -0,0 +1,3 @@
+export type Qiita = {
+  title: string;
+};
---
+++ src/routes/qiita.tsx
@@ -0,0 +1,42 @@
+import type { Component } from "solid-js";
+import { createResource, For, createSignal, Suspense } from "solid-js";
+import { Cat } from "../types";
+import { Qiita } from "../qiita-types";
+
+const fetchData = async (skip: number) =>
+  (await fetch(`https://qiita.com/api/v2/tags/react/items`)).json();
+
+const CatsPage: Component = () => {
+  const [skip, setSkip] = createSignal(0);
+  const [data, { refetch }] = createResource<Qiita[], number>(skip, fetchData);
+
+  let input!: HTMLInputElement;
+
+  const onSetSkip = () => {
+    if (!input.value.trim()) return;
+    if (isNaN(Number(input.value))) return;
+    setSkip(Number(input.value));
+  };
+
+  return (
+    <>
+      <h1>Qiita Page</h1>
+      <input type="number" placeholder="Enter Skip Number" ref={input} />
+      <button onClick={() => onSetSkip()}>set skip</button>
+      <button onClick={() => refetch()}>refetch</button>
+      <Suspense fallback={<div>Loading...</div>}>
+        <ul>
+          <For each={data()}>
+            {(qiita) => (
+              <li>
+                  ${qiita.title}
+              </li>
+            )}
+          </For>
+        </ul>
+      </Suspense>
+    </>
+  );
+};
+
+export default CatsPage;
