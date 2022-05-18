02bb004fbb1606783d73e19d2f8b54f5804dca97
hiroyuki12 <hiroyuki12@gmail.com>
2022-05-18 22:10:20 +0900

    Add prev page button, show page number Qiita.

--- src/routes/qiita.tsx
+++ src/routes/qiita.tsx
@@ -13,11 +13,17 @@ const QiitaPage: Component = () => {
 
   let input!: HTMLInputElement;
 
-  const onSetSkip = () => {
+  const onNextPage= () => {
     //if (!input.value.trim()) return;
     //if (isNaN(Number(input.value))) return;
     //setPage(Number(input.value));
     setPage((prevCount) => prevCount + 1);
+  };
+  const onPrevPage= () => {
+    //if (!input.value.trim()) return;
+    //if (isNaN(Number(input.value))) return;
+    //setPage(Number(input.value));
+    setPage((prevCount) => prevCount - 1);
   };
 
   return (
@@ -31,8 +37,10 @@ const QiitaPage: Component = () => {
         <a className="QiitaApp-link" href="https://mbp.hatenablog.com/entry/2021/07/15/214300" target="_blank">ReactでQiita APIから記事情報を取得して表示</a>
       </p>
       <input type="number" placeholder="Enter Skip Number" ref={input} />
-      <button onClick={() => onSetSkip()}>next page</button>
+      <button onClick={() => onNextPage()}>next page</button>
+      <button onClick={() => onPrevPage()}>prev page</button>
       <button onClick={() => refetch()}>refetch</button>
+      page: {page}
       <Suspense fallback={<div>Loading...</div>}>
         <ul>
           <For each={data()}>
