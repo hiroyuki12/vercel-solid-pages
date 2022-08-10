a39059d439bd62d04df9bd87a85e84592c18d49f
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-11 07:50:43 +0900

    Add vite-plugin-pwa.

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import solid from "solid-start";
 import vercel from "solid-start-vercel"
+import { VitePWA } from 'vite-plugin-pwa';
 
 export default defineConfig({
   server: {
     open: true,
   },
-  plugins: [solid({ adapter: vercel() })]
+  plugins: [solid({ adapter: vercel() }), VitePWA()]
 });
