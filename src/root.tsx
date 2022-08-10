bbfad133ad68cdd6f5bc06b0710dc8d1f49546e1
hiroyuki12 <hiroyuki12@gmail.com>
2022-08-11 08:29:15 +0900

    Add manifest.json, icon.

--- src/root.tsx
+++ src/root.tsx
@@ -8,6 +8,8 @@ export default function Root() {
     <html lang="en">
       <head>
         <meta charset="utf-8" />
+        <link rel="manifest" href="/manifest.json" />
+        <link rel="apple-touch-icon" href="/icon.png">
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <Meta />
         <Links />
